ee3e447a77974a87bf0d4c5469bb9fe9d7b60db6
Andżelika Domańska <adomanska96@gmail.com>
2018-01-06 21:09:48 +0100

    Add template files

---
+++ Language School Web/Controllers/ClassesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using LanguageSchool.Models;
+
+namespace Language_School_Web.Controllers
+{
+    public class ClassesController : Controller
+    {
+        // GET: Classes
+        public ActionResult Index()
+        {
+            IEnumerable<ClassDataDto> classes = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://projektnet.mini.pw.edu.pl/LanguageSchool/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("classes");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<ClassDataDto>>();
+                    readTask.Wait();
+
+                    classes = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    classes = Enumerable.Empty<ClassDataDto>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(classes);
+        }
+    }
+}
---
+++ Language School Web/Models/ClassDataDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageSchool.Models
+{
+    public class ClassDataDto
+    {
+        public int Id { get; set; }
+        public string ClassName { get; set; }
+        public string Language { get; set; }
+        public string LanguageLevel { get; set; }
+        public int StudentsCount { get; set; }
+        public int StudentsMax { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string Price { get; set; }
+    }
+}
