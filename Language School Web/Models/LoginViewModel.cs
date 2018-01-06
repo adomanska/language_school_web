3edbe7c6d9b015a4f763b80ef7d923e99147d753
Beata <bkakareko@gmail.com>
2018-01-06 21:08:16 +0100

    Simple login action

---
+++ Language School Web/Models/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Language_School_Web.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Login")]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        public const string Grant_Type = "password";
+    }
+}
